1e73378bfa5413979bcd81b4745e5d8b31bfc467
devika0999 <krishnadevika445@gmail.com>
2022-10-27 10:58:10 +0530

    nest app backend

---
+++ src/main/java/com/nestdigital/nestApp_backend/NestAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.nestApp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/nestApp_backend/NestAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.nestApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
